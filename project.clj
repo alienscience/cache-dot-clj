6959cf49bc8760841e71b4bc43afed3d779acea7
Alien Science <saul@alienscience.org.uk>
2010-07-03 09:58:49 +0100

    Added license declaration to POM

--- project.clj
+++ project.clj
@@ -4,5 +4,9 @@
   :dependencies []
   :dev-dependencies [[org.clojure/clojure "1.1.0"]
                      [org.clojure/clojure-contrib "1.1.0"]
-                     [swank-clojure "1.2.0"]])
+                     [swank-clojure "1.2.0"]]
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "same as Clojure"})
 
