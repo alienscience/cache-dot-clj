3ea9d6a239aeac455443116a19e0b7051dd7f4fe
Ben Mabey <ben@benmabey.com>
2011-05-05 11:10:24 -0600

    adds log4j for dev so we can see cache stats in tests

--- ehcache/project.clj
+++ ehcache/project.clj
@@ -5,7 +5,8 @@
   :dev-dependencies [[org.clojure/clojure "1.2.0"]
                      [org.clojure/clojure-contrib "1.2.0"]
                      [swank-clojure "1.2.1"]
-                     [org.slf4j/slf4j-simple "1.5.11"]
+                     [log4j/log4j "1.2.13"]
+                     [org.slf4j/slf4j-log4j12 "1.5.6"]
                      [clj-file-utils  "0.2.1"]]
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
