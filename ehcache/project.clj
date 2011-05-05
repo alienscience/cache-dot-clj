43ff3fef56711749ad54915e8ffb8f4770c6e152
Ben Mabey <ben@benmabey.com>
2011-05-05 15:44:01 -0600

    excludes log4j from ehcache jar

--- ehcache/project.clj
+++ ehcache/project.clj
@@ -9,6 +9,7 @@
                      [org.slf4j/slf4j-log4j12 "1.5.6"]
                      [clj-file-utils  "0.2.1"]]
   :warn-on-reflection false
+  :jar-exclusions [#"log4j\.properties"]
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
